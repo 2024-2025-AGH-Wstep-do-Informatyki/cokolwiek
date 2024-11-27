35f3aa19c0f809f8babf8591b51e81d67bdc0792
Mateusz Klikuszewski <mateuszklikuszewski@gmail.com>
2024-11-27 18:35:36 +0100

    Wypisywanie wyniku

--- insert.py
+++ insert.py
@@ -1,10 +1,3 @@
-equation = input()
-stack=[]
-onp = []
-i = 0
-l = 0
-ws = 0
-
 def number(i):
     global l
     if i + 1 < len(equation) and '0' <= equation[i + 1] <= '9':
@@ -13,10 +6,18 @@ def number(i):
     else:
         return equation[i]
 
+equation = input()
+stack=[]
+onp = []
+i = 0
+l = 0
+ws = 0
+dzialania = {'+','-','*','/','^'}
+
 while i<len(equation):
     if equation[i] >= '0' and equation[i] <= '9': #cyfra
         l = 0
-        onp.append(number(i))
+        onp.append(int(number(i)))
         i += l
     elif equation[i] == '+' or equation[i] == '-': #dodawanie odejmowanie
         while ws and stack[ws - 1] != '(':
@@ -54,4 +55,22 @@ while ws:
     ws -= 1
     onp.append(stack[ws])
     stack.pop(ws)
+print(onp)
+wynik = 0
+i = 0
+while len(onp) > 1:
+    if onp[i] in dzialania:
+        if onp[i] =='+':
+            onp[i-2]+=onp[i-1]
+        elif onp[i] =='-':
+            onp[i-2]-=onp[i-1]
+        elif onp[i] =='*':
+            onp[i-2]*=onp[i-1]
+        elif onp[i] =='/':
+            onp[i-2]/=onp[i-1]
+        onp.pop(i)
+        onp.pop(i - 1)
+        i-=2
+
+    i+=1
 print(onp)
